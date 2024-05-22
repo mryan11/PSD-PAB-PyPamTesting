0900354b42b7b3f6caa40a8d68b7ffcae06acbc0
Michael.C Ryan <michael.c.ryan@noaa.gov>
2024-05-22 13:20:19 -0400

    Added files to test.
    Added files to test.

---
+++ pypam/process_wave.py
@@ -0,0 +1,15 @@
+from pypam import acoustic_file
+import pyhydrophone as pyhy
+
+# SoundTrap
+model = 'ST600HF'
+name = 'SoundTrap'
+serial_number = 5459
+soundtrap = pyhy.soundtrap.SoundTrap(name=name, model=model, serial_number=serial_number)
+
+acu_file = acoustic_file.AcuFile("5459_20220118_052541.wav", soundtrap, 1)
+acu_file.plot_power_spectrum()
+
+nfft = 8000  # Set to the same as sampling rate (or higher band limit, when downsampling) for 1s time resolution
+acu_file.hybrid_millidecade_bands(nfft=nfft, fft_overlap=0.5, binsize=None, bin_overlap=0, db=True,
+                                               method='density', band=None)
